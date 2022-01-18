e883a181e8dbab32fe45a97a9a10913b3e3cf485
Philip Beauford <PhilipBeauford@github.com>
2022-01-18 00:04:19 -0800

    updates

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Head from "next/head";
 export default function Home() {
   return (
     <div className="flex justify-center items-center h-screen">
-      <h1 className="">Welcome to Bare NextJS + Tailwind CSS!</h1>
+      <h1 className="">Welcome to Bare NextJS & Tailwind CSS!</h1>
     </div>
   );
 }
